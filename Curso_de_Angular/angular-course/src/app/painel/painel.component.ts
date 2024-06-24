f380b4acd80a004a853eb72219e60ac69cc18dbd
Inajara <inajaralemos1@gmail.com>
2024-06-24 20:55:04 -0300

    retomada e correções curso de angular

--- Curso_de_Angular/angular-course/src/app/painel/painel.component.ts
+++ Curso_de_Angular/angular-course/src/app/painel/painel.component.ts
@@ -15,8 +15,19 @@ export class PainelComponent {
     public instrucao: string = 'Traduza a frase:'
     public frases: Frase[] = FRASES
     public resposta: string = ''
+    public rodada : number = 0
+    public rodadaFrase : Frase
+
+    constructor(){
+        this.rodadaFrase = this.frases[this.rodada]
+    }
 
     public atualizaResposta(resposta: Event): void{
         this.resposta = (<HTMLInputElement>resposta.target).value
+    }
+
+    public verificaResposta(): void{
+        this.rodada++
+        this.rodadaFrase = this.frases[this.rodada]
     }
 }
